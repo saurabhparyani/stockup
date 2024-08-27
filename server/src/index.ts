562f23a260b29ff68c5383c560b800ad98074fe8
Saurabh Paryani <saurabhparyani64@gmail.com>
2024-08-28 04:48:39 +0530

    added basic backend functionality and setup

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors'
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+// ROUTE IMPORTS
+
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+}));
+app.use(morgan("common"));
+app.use(cors());
+
+// ROUTES
+app.get("/hello", (req,res) => {
+    res.send("Hello World");
+})
+
+// SERVER
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
+
+
